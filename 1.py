57128696bb77cec921ec41cac262111553f7b616
varunj546 <varunjavali00@gmail.com>
2026-07-14 21:15:09 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,3 +6,21 @@ is_raining = False
 print(city)
 print(temperature)
 print(is_raining)
+
+2. Data Types in Python
+Explanation
+A data type tells Python what kind of value is stored in a variable.
+int → Whole numbers
+float → Decimal numbers
+str → Text
+bool → True or False
+Example
+marks = 95
+height = 5.4
+college = "Srinivas University"
+passed = True
+
+print(type(marks))
+print(type(height))
+print(type(college))
+print(type(passed))

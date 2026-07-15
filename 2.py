db593994d88964ea243fefbc17bf4cff93fec5fb
varunj546 <varunjavali00@gmail.com>
2026-07-15 22:26:50 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,10 @@
+7. Input and Output in Python
+7.1 Taking Input from the User
+Explanation
+The input() function allows the user to enter information while the program is running. Whatever the user types is stored as a string by default. If you need a number, convert it using int() or float().
+Example
+student = input("Enter your name: ")
+semester = int(input("Enter your semester: "))
+
+print(student)
+print(semester)

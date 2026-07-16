bb8240579c6768070be375d4079a7119de3ae2bc
varunj546 <varunjavali00@gmail.com>
2026-07-16 21:54:23 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,18 @@
+1. Assignment Operators
+Explanation:
+Assignment operators are used to store or update values in a variable.
+Example:
+salary = 15000
+print("Salary:", salary)
+
+salary += 5000
+print("After Bonus:", salary)
+
+salary -= 2000
+print("After Deduction:", salary)
+
+salary *= 2
+print("Double Salary:", salary)
+
+salary //= 3
+print("After Floor Division:", salary)

a6d5b5f4c45dbf7152315f7604b706a0ea5a1a2e
varunj546 <varunjavali00@gmail.com>
2026-07-17 22:31:05 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,19 @@
+1. Assignment Operators
+Explanation:
+Assignment operators are used to store a value in a variable or update the value after performing an operation.
+Code:
+marks = 50
+print("Initial Marks:", marks)
+
+marks += 20
+print("After Adding:", marks)
+
+marks -= 10
+print("After Subtracting:", marks)
+
+marks *= 2
+print("After Multiplying:", marks)
+
+marks %= 15
+print("Remainder:", marks)
+

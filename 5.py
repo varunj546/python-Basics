e90134ab0e256059a394f2d63244a36a2453a850
varunj546 <varunjavali00@gmail.com>
2026-07-18 23:02:54 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,6 @@
+1. What is a List?
+Explanation:
+A list is a collection of multiple values stored in a single variable. Lists are ordered, changeable (mutable), and can contain duplicate values and different data types.
+Code:
+student = ["Rahul", 21, "AIML", True]
+print(student)

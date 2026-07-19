e65ed6737e77b02d14c22d69476f9760d015bab8
varunj546 <varunjavali00@gmail.com>
2026-07-19 22:53:17 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,9 @@
+1. Tuples in Python
+Explanation:
+A tuple is a collection of values stored in a single variable. Tuples are ordered but cannot be changed after they are created. They can store different types of data.
+Code:
+student = ("Anjali", 20, "AIML", 8.9)
+
+print(student)
+print(student[0])
+print(student[-1])

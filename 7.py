cc880d04304cbff480468b52d8432af10bab3d83
varunj546 <varunjavali00@gmail.com>
2026-07-20 23:05:57 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,11 @@
+1. Creating a Dictionary
+Explanation:
+A dictionary stores data in key-value pairs. Each key is unique and is used to access its corresponding value.
+Code:
+student = {
+    "Name": "Rahul",
+    "Age": 21,
+    "Course": "AIML"
+}
+
+print(student)

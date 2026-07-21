67f1c168fc03aef6308e6a106ed3a2a42a85be84
varunj546 <varunjavali00@gmail.com>
2026-07-21 23:05:35 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,18 @@
+1. The if Statement
+Explanation:
+The if statement checks whether a condition is True. If it is, the code inside the if block is executed.
+Code:
+temperature = 32
+
+if temperature > 30:
+    print("The weather is hot.")
+2. The else Statement
+Explanation:
+The else statement runs when the if condition is False.
+Code:
+password = "python123"
+
+if password == "admin123":
+    print("Login Successful")
+else:
+    print("Invalid Password")

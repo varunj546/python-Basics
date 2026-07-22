a18b89afe505d96a28d27fc956205a7fe1f0125d
varunj546 <varunjavali00@gmail.com>
2026-07-22 23:11:09 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,18 @@
+1. Basic while Loop
+Explanation:
+A while loop repeats a block of code until the condition becomes False.
+Code:
+count = 1
+
+while count <= 5:
+    print("Welcome")
+    count += 1
+2. Example: Printing Even Numbers
+Explanation:
+The loop prints only even numbers from 2 to 10.
+Code:
+num = 2
+
+while num <= 10:
+    print(num)
+    num += 2

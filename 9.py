a59c6614d6603928d3824c2d1048a4f579742d50
varunj546 <varunjavali00@gmail.com>
2026-07-22 23:11:41 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -16,3 +16,25 @@ num = 2
 while num <= 10:
     print(num)
     num += 2
+
+3. Avoiding Infinite Loops
+Explanation:
+Always update the loop variable. Otherwise, the condition never becomes False and the loop runs forever.
+Code:
+number = 1
+
+while number <= 3:
+    print(number)
+    number += 1
+4. Using break
+Explanation:
+The break statement immediately stops the loop when a specific condition is met.
+Code:
+num = 1
+
+while num <= 10:
+    if num == 7:
+        print("Loop Stopped")
+        break
+    print(num)
+    num += 1

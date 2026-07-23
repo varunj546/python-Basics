dc5ed7c25fac80883a16ccf2e8b53747625faaa3
varunj546 <varunjavali00@gmail.com>
2026-07-23 23:17:24 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -128,3 +128,12 @@ if ch.lower() in "aeiou":
     print("Vowel")
 else:
     print("Consonant")
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive Number")
+elif num < 0:
+    print("Negative Number")
+else:
+    print("Zero")

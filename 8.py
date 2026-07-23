a48c15fa5b866b052a02cb9e6b555dc11454340f
varunj546 <varunjavali00@gmail.com>
2026-07-23 23:16:55 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -112,3 +112,11 @@ match fruit:
         print("Orange is Orange.")
     case _:
         print("Fruit Not Found")
+
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")

4fbe91d629d256874c2c0ec83c91f6412affa871
varunj546 <varunjavali00@gmail.com>
2026-07-24 23:33:58 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -108,3 +108,13 @@ i = 1
 while i <= 10:
     print(num, "x", i, "=", num * i)
     i += 1
+
+num = int(input("Enter a number: "))
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num //= 10
+
+print("Reversed Number =", reverse)

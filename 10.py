759f9fa3fdca9c560c1355ec330a37ba25e76b67
varunj546 <varunjavali00@gmail.com>
2026-07-28 22:19:29 +0530

    Implement multiplication table for user input number
    Added functionality to multiply a user-input number by 1 to 10.

--- 10.py
+++ 10.py
@@ -38,3 +38,7 @@ for i in range(1, 11):
 for i in range(1, 51):
     if i % 2 == 0:
         print(i)
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)

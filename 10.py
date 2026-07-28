e455533c907ad3fb5ebda0f8f4b73ecc28f84726
varunj546 <varunjavali00@gmail.com>
2026-07-28 22:23:18 +0530

    Add pattern printing for specified number of rows

--- 10.py
+++ 10.py
@@ -69,3 +69,9 @@ for i in range(1, rows + 1):
     for j in range(i):
         print("*", end=" ")
     print()
+rows = 5
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()

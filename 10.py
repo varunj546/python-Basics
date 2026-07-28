1886523eff601cfd1c4762ae87e88a33fed6d5d2
varunj546 <varunjavali00@gmail.com>
2026-07-28 22:21:41 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -62,3 +62,10 @@ for num in numbers:
         largest = num
 
 print("Largest number:", largest)
+
+rows = 5
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()

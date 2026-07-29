4607d352b627f8381eb14adf73b033f00bb467dc
varunj546 <varunjavali00@gmail.com>
2026-07-29 23:20:31 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -105,3 +105,11 @@ for i in range(1, 4):
     for j in range(1, 4):
         print(i, "*", j, "=", i * j)
     print()
+5. Using break
+numbers = [5, 10, 15, 20, 25]
+
+for num in numbers:
+    if num == 20:
+        print("Number Found")
+        break
+    print(num)

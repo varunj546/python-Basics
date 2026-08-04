635931059a732e3d0c78c4cbbe1487138406dc68
varunj546 <varunjavali00@gmail.com>
2026-08-04 23:11:16 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -159,3 +159,10 @@ if num % 2 == 0:
     print("Even Number")
 else:
     print("Odd Number")
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")

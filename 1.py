d50b7276b5516d701e25fcbe94ca04eff7de5096
varunj546 <varunjavali00@gmail.com>
2026-08-04 23:11:33 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -166,3 +166,10 @@ if num % 2 == 0:
     print("Even Number")
 else:
     print("Odd Number")
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")

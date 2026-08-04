b18a595a15f1d7acca10ec9dc2917df7b331742f
varunj546 <varunjavali00@gmail.com>
2026-08-04 21:35:34 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -82,3 +82,22 @@ print("Division =", length / width)
 print("Floor Division =", length // width)
 print("Modulus =", length % width)
 print("Power =", length ** 2)
+
+1. Assignment Operators
+Explanation:
+Assignment operators are used to store a value in a variable or update the value after performing an operation.
+Code:
+marks = 50
+print("Initial Marks:", marks)
+
+marks += 20
+print("After Adding:", marks)
+
+marks -= 10
+print("After Subtracting:", marks)
+
+marks *= 2
+print("After Multiplying:", marks)
+
+marks %= 15
+print("Remainder:", marks)

20bdbbc5be79c630ceb5822f819e4b3f9768bf11
varunj546 <varunjavali00@gmail.com>
2026-08-06 22:18:31 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -151,3 +151,13 @@ cat = Cat()
 
 dog.sound()
 cat.sound()
+
+class Bank:
+    def __init__(self):
+        self.__balance = 5000
+
+    def show_balance(self):
+        print("Balance:", self.__balance)
+
+bank1 = Bank()
+bank1.show_balance()

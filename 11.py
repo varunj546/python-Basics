ee9790355c5cd336844ded8c08dca4f32bf012b8
varunj546 <varunjavali00@gmail.com>
2026-08-06 22:19:08 +0530

    Add Vehicle and Car classes with start and drive methods

--- 11.py
+++ 11.py
@@ -161,3 +161,16 @@ class Bank:
 
 bank1 = Bank()
 bank1.show_balance()
+
+class Vehicle:
+    def start(self):
+        print("Vehicle is starting")
+
+class Car(Vehicle):
+    def drive(self):
+        print("Car is driving")
+
+car1 = Car()
+car1.start()
+car1.drive()
+
